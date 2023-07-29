b26aa95212993412eeeed85e204619f882b8759f
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-07-29 22:06:06 +0700

    ноу комментс

--- libs/IRCParser/IRCParser.cpp
+++ libs/IRCParser/IRCParser.cpp
@@ -57,80 +57,83 @@ EXPORT char* CALLBACK ParseLine(char* original_line, int length) {
 	char debug_parsed_line[4096];
 	char parsed_line[4096];
 	char words[512][512];
-	//try {
-		char prefix[80];
-		char command[60];
-		char params[256];
-		char body[1536];
-		char* token = strtok(line, " ");
-		int spaces = 0;
-		int body_index;
-		while(token != NULL) {
-			if(token[0] == ':') {
-				sprintf(words[spaces], token + 1);
-			} else {
-				sprintf(words[spaces], token);
-			}
-			if(spaces == 0) {
-				sprintf(prefix, words[spaces]);
-			} else if(spaces == 1) {
-				sprintf(command, words[spaces]);
-			} else if(spaces == 2) {
-				sprintf(params, words[spaces]);
-			} else if(spaces == 3) {
-				sprintf(body, words[spaces]);
-				body_index = strlen(body);
-			} else {
-				body_index += sprintf(body + body_index, " %s", words[spaces]);
-			}
-			token = strtok(NULL, " ");
-			spaces++;
-		}
-		if(spaces > 2) {
-			if(strcmp(command, "372") == 0) {
-				if(original_line[0] == ':') {
-					sprintf(body, original_line + 
-						strlen(prefix) + strlen(command) + strlen(params) + 5);
+	if(strlen(original_line) > 0) {
+		try {
+			char prefix[80];
+			char command[60];
+			char params[256];
+			char body[1536];
+			char* token = strtok(line, " ");
+			int spaces = 0;
+			int body_index;
+			while(token != NULL) {
+				if(token[0] == ':') {
+					sprintf(words[spaces], token + 1);
 				} else {
-					sprintf(body, original_line + 
-						strlen(prefix) + strlen(command) + strlen(params) + 4);
+					sprintf(words[spaces], token);
 				}
-				body[strlen(original_line) - 
-					(strlen(prefix) + strlen(command) + strlen(params) + 5)] = '\0';
-				sprintf(parsed_line, "[MOTD] %s\r\n", body);
-			} else if(strcmp(command, "396") == 0) {
-				sprintf(parsed_line, "[396] %s@%s\r\n", params, body);
-			} else if(strcmp(command, "001") == 0) {
-				sprintf(parsed_line, "[001] %s\r\n", body);
-			} else if(strcmp(command, "002") == 0) {
-				sprintf(parsed_line, "[002] %s\r\n", body);
-			} else if(strcmp(command, "003") == 0) {
-				sprintf(parsed_line, "[003] %s\r\n", body);
-			} else if(strcmp(command, "004") == 0) {
-				sprintf(parsed_line, "[004] %s\r\n", body);
-			} else if(strcmp(command, "005") == 0) {
-				sprintf(parsed_line, "[005] %s\r\n", body);
-			} else if(strcmp(command, "End") == 0) {
-				sprintf(parsed_line, "---------------------------------------------------\r\n");
-			} else if(strcmp(prefix, "ERROR") == 0) {
-				sprintf(parsed_line, "[%s] %s %s\r\n", prefix, command, body);
-			} else if(strlen(command) == 3 && isdigit(command[0]) != 0 
-				&& isdigit(command[1]) != 0 && isdigit(command[2]) != 0) {
-				sprintf(parsed_line, "[%s] %s %s\r\n", command, params, body);
+				if(spaces == 0) {
+					sprintf(prefix, words[spaces]);
+				} else if(spaces == 1) {
+					sprintf(command, words[spaces]);
+				} else if(spaces == 2) {
+					sprintf(params, words[spaces]);
+				} else if(spaces == 3) {
+					sprintf(body, words[spaces]);
+					body_index = strlen(body);
+				} else {
+					body_index += sprintf(body + body_index, " %s", words[spaces]);
+				}
+				token = strtok(NULL, " ");
+				spaces++;
+			}
+			if(spaces > 2) {
+				if(strcmp(command, "372") == 0) {
+					if(original_line[0] == ':') {
+						sprintf(body, original_line + 
+							strlen(prefix) + strlen(command) + strlen(params) + 5);
+					} else {
+						sprintf(body, original_line + 
+							strlen(prefix) + strlen(command) + strlen(params) + 4);
+					}
+					body[strlen(original_line) - 
+						(strlen(prefix) + strlen(command) + strlen(params) + 5)] = '\0';
+					sprintf(parsed_line, "[MOTD] %s\r\n", body);
+				} else if(strcmp(command, "396") == 0) {
+					sprintf(parsed_line, "[396] %s@%s\r\n", params, body);
+				} else if(strcmp(command, "001") == 0) {
+					sprintf(parsed_line, "[001] %s\r\n", body);
+				} else if(strcmp(command, "002") == 0) {
+					sprintf(parsed_line, "[002] %s\r\n", body);
+				} else if(strcmp(command, "003") == 0) {
+					sprintf(parsed_line, "[003] %s\r\n", body);
+				} else if(strcmp(command, "004") == 0) {
+					sprintf(parsed_line, "[004] %s\r\n", body);
+				} else if(strcmp(command, "005") == 0) {
+					sprintf(parsed_line, "[005] %s\r\n", body);
+				} else if(strcmp(command, "End") == 0) {
+					sprintf(parsed_line, "---------------------------------------------------\r\n");
+				} else if(strcmp(prefix, "ERROR") == 0) {
+					sprintf(parsed_line, "[%s] %s %s\r\n", prefix, command, body);
+				} else if(strlen(command) == 3 && isdigit(command[0]) != 0 
+					&& isdigit(command[1]) != 0 && isdigit(command[2]) != 0) {
+					sprintf(parsed_line, "[%s] %s %s\r\n", command, params, body);
+				} else {
+					sprintf(parsed_line, "[%s] %s\r\n", command, body);
+				} 
+			} else if(spaces > 0) {
+				sprintf(parsed_line, "[RAW] [%s]\r\n", original_line);
 			} else {
-				sprintf(parsed_line, "[%s] %s\r\n", command, body);
-			} 
-		} else if(spaces > 0) {
-			sprintf(parsed_line, "[RAW] [%s]\r\n", original_line);
-		} else {
-			sprintf(parsed_line, "[Parsing error | "
-				"Size: %d chars / %d spaces]\r\n", strlen(original_line), spaces);
+				sprintf(parsed_line, "[Parsing error]\r\n");
+			}
+			return parsed_line;
+		} catch(...) {
+			sprintf(parsed_line, "[Parsing error]\r\n");
+			return parsed_line;
 		}
+	} else {
 		return parsed_line;
-	//} catch(...) {
-		//sprintf(parsed_line, "[Parsing error]\r\n");
-		//return parsed_line;
-	//}
+	}
 }
 
 EXPORT char* CALLBACK ParsePacket(char original_packet[4096]) {
